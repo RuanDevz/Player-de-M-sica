d5cabe80417c187e1a7411fe52f8cec0d206b72b
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-07 10:40:48 -0300

    vamo la

---
+++ imagem/main.js
@@ -0,0 +1,13 @@
+const play = document.getElementById('play')
+
+play.addEventListener('click', start)
+
+function start(){
+    if(play.classList.contains('fa-play')){
+        play.classList.remove('fa-play');
+        play.classList.add('fa-pause');
+    } else {
+        play.classList.remove('fa-pause');
+        play.classList.add('fa-play');
+    }
+}
