f5953e38ca1bde5b55a030d06681ac8a57380c85
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-08 11:48:04 -0300

    complicado

--- imagem/main.js
+++ imagem/main.js
@@ -1,13 +1,23 @@
-const play = document.getElementById('play')
+const play = document.getElementById('play');
 
-play.addEventListener('click', start)
+play.addEventListener('click', start);
 
-function start(){
-    if(play.classList.contains('fa-play')){
+function start() {
+    if (play.classList.contains('fa-play')) {
         play.classList.remove('fa-play');
         play.classList.add('fa-pause');
+        document.querySelector('.barra #audio').play();
     } else {
         play.classList.remove('fa-pause');
-        play.classList.add('fa-play')
+        play.classList.add('fa-play');
+        document.querySelector('.barra #audio').pause();
     }
+}
+
+let musica = document.getElementById('audio')
+musica.addEventListener('timeupdate',Barra)
+
+function Barra(){
+    let barra = document.querySelector('progress')
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
 }
